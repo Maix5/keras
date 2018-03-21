7034f43000231f5c8610a63ab7ace4ce4de95f38
Maix5 <mariusblaz@gmail.com>
2018-03-22 00:54:20 +0200

    Removed unintended change.

--- keras/engine/topology.py
+++ keras/engine/topology.py
@@ -736,7 +736,7 @@ class Layer(object):
                 Shape tuples can include None for free dimensions,
                 instead of an integer.
 
-        # Returns2, 16, 3, True
+        # Returns
             An input shape tuple.
         """
         if hasattr(self, 'get_output_shape_for'):
